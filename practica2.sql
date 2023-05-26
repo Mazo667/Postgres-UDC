38f01b4711a7f2c168bd1c43358b843434407b5c
Maximiliano <68477400+Mazo667@users.noreply.github.com>
2023-05-26 07:23:01 -0300

    Ya cargado todo correctamente

--- practica2.sql
+++ practica2.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE practica2 WITH ENCODING = 'UTF8' CONNECTION LIMIT -1;
 
-CREATE TABLE cantante(id serial NOT NULL,
+CREATE TABLE cantante(
+id serial NOT NULL,
 nombre character varying(30),
 CONSTRAINT pk_id
 PRIMARY KEY (id) );
@@ -10,8 +11,18 @@ id serial NOT NULL,
 titulo character varying(30), 
 cantante integer, 
 duracion integer, 
-CONSTRAINT pk_id 
-PRIMARY KEY (id),
-CONSTRAINT fk_cantante 
-FOREIGN KEY (cantante)
-REFERENCES cantante (id) );
+CONSTRAINT pk_cds PRIMARY KEY (id),
+CONSTRAINT fk_cantante FOREIGN KEY (cantante) REFERENCES cantante (id)
+);
+
+INSERT INTO cantante (nombre) VALUES ('BSO');
+INSERT INTO cantante (nombre) VALUES ('Mike Oldfield');
+INSERT INTO cantante (nombre) VALUES ('Sting');
+INSERT INTO cantante (nombre) VALUES ('Madonna');
+
+INSERT INTO cds (titulo, cantante, duracion) VALUES ('Lord of the Rings', 1, 54);
+INSERT INTO cds (titulo, cantante, duracion) VALUES ('Star Wars', 1, 57);
+INSERT INTO cds (titulo, cantante, duracion) VALUES ('Tubular Bells', 2, 73);
+INSERT INTO cds (titulo, cantante, duracion) VALUES ('Elements', 2, 68);
+INSERT INTO cds (titulo, cantante, duracion) VALUES ('Sting & The Police', 3, 56);
+INSERT INTO cds (titulo, cantante, duracion) VALUES ('Ray of light', 4, 55);

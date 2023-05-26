282e64bb2b15282a90a5e91bb7f8eb9a87970016
Maximiliano <68477400+Mazo667@users.noreply.github.com>
2023-05-25 21:22:34 -0300

    Inserto Valores a las tablas

--- practica1.sql
+++ practica1.sql
@@ -1,6 +1,8 @@
 CREATE DATABASE "practica1" 
 WITH ENCODING = 'UTF8' 
 CONNECTION LIMIT =-1;
+
+--A tener en cuenta los nombres se pueden poner con comillas dobles para que tome las mayusculas sino por defecto escribe en miniscula
 --Creo la Tabla Dispositivo
 
 CREATE TABLE dispositivo (
@@ -19,5 +21,23 @@ CONSTRAINT pk_id
 PRIMARY KEY (id), 
 CONSTRAINT fk_dispositivo FOREIGN KEY (dispositivo) 
 REFERENCES dispositivo (id) );
+
+--Añado 3 Dispositivos
+INSERT INTO dispositivo (nombre) VALUES ('Notebook');
+INSERT INTO dispositivo (nombre) VALUES ('Ipad');
+INSERT INTO dispositivo (nombre) VALUES ('Celular');
+
+--Añado 5 aplicaciones en relacion a las existentes
+INSERT INTO aplicacion (nombre,tamanio,dispositivo) VALUES ('Gestion',1500,1);
+INSERT INTO aplicacion (nombre,tamanio,dispositivo) VALUES ('Chess',160,2);
+INSERT INTO aplicacion (nombre,tamanio,dispositivo) VALUES ('Frozen',385,2);
+INSERT INTO aplicacion (nombre,tamanio,dispositivo) VALUES ('Chess 3D',87,3);
+INSERT INTO aplicacion (nombre,tamanio,dispositivo) VALUES ('GPS',275,3);
+
+--Modifico los datos de Dispositivo
+UPDATE dispositivo SET nombre = 'Tablet Ipad 64gb' WHERE nombre = 'Ipad';
+--Modifico los datos de APlicacion
+UPDATE  aplicacion SET tamanio = 96 WHERE nombre = 'Chess 3D';
+
 
 
